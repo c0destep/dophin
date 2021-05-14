4ff3f6cc78926d9242f8d062bafb8cbd6705b60c
Lucas Alves <phytolinedev@gmail.com>
2021-05-14 00:57:11 -0300

    bug fix
    - Remoção do menu mobile devido ao não funcionamento correto.
    - Remoção menu superior fixo devido há bug visual.
    + Adicionado favicon na page.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,9 @@
-$('.masthead').visibility({
-    once: false,
-    onBottomPassed: function () {
-        $('.fixed.menu').transition('fade in');
-    },
-    onBottomPassedReverse: function () {
-        $('.fixed.menu').transition('fade out');
-    }
-});
-
-$('.ui.sidebar').sidebar('attach events', '.toc.item');
-
 //<![CDATA[preloader
 $(window).on('load', function () {
-  $('#preloader .inner').fadeOut();
-  $('#preloader').delay(350).fadeOut('slow'); 
-  $('body').delay(350).css({'overflow': 'visible'});
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
 })
 //]]>
-
-
